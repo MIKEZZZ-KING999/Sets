38eb7085c4dbf27504922471b1150db4bceec2b4
MIKEZZZ-KING999 <micigwubor@gmail.com>
2024-11-28 17:57:03 +0000

    Add files via upload

---
+++ Set.py
@@ -0,0 +1,11 @@
+sample_list = [1,2,3,4,5,6,6,5,4,3,2,1]
+
+sample_set = set(sample_list)
+print(sample_set)
+
+print(sample_set[2])
+if 4 in sample_set:
+    print("Yes")
+else:
+    print("No")
+      
